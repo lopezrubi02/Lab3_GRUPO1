311fe011523b580e8f60bc2285e8deb875597e19
Emerson Munoz <munoz.e@pucp.edu.pe>
2021-04-26 19:46:38 -0500

    primer commit - inicializando Employee

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,33 +1,50 @@
 package com.example.laboratorio3.controller;
 
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 //COMPLETAR
+@Controller
+@RequestMapping("/employee")
 public class EmployeeController {
 
     //COMPLETAR
+    @Autowired
+    EmployeesRepository employeesRepository;
 
-    public String listaEmployee(   ){
+    @GetMapping(value = {"", "/"})
+    public String listaEmployee(Model model){
         //COMPLETAR
+        model.addAttribute("listaEmpleados", employeesRepository.findAll());
+        return "employee/lista";
     }
 
     public String nuevoEmployeeForm( ) {
         //COMPLETAR
+        return "0";
     }
 
 
     public String guardarEmployee() {
         //COMPLETAR
+        return "0";
     }
 
 
     public String editarEmployee() {
         //COMPLETAR
+        return "0";
     }
 
 
     public String borrarEmpleado() {
 
        //COMPLETAR
-
+        return "0";
     }
 
     //COMPLETAR
--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -6,7 +6,128 @@ import javax.persistence.*;
 import java.util.Date;
 
 //COMPLETAR
+@Entity
+@Table(name="employees")
 public class Employees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String hireDate;
+    private String jobId;
+    private String salary;
+    private Double commissionPct;
+    private int managerId;
+    private int departmentId;
+    private int enabled;
 
-           //COMPLETAR
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public Double getCommissionPct() {
+        return commissionPct;
+    }
+
+    public void setCommissionPct(Double commissionPct) {
+        this.commissionPct = commissionPct;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public int getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(int enabled) {
+        this.enabled = enabled;
+    }
+
+    //COMPLETAR
 }
