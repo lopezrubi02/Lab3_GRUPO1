91888f0347bd1ea4a1d27eddeed3d0a46df3bda2
Emerson Munoz <munoz.e@pucp.edu.pe>
2021-04-28 02:48:15 -0500

    Arreglos 2

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -102,7 +102,7 @@ public class EmployeeController {
     //COMPLETAR
     @PostMapping("/BuscarEmpleados")
     public String buscarEmpleado(@RequestParam("searchField") String searchField, Model model){
-        List<Employees> listaEmpleados = employeesRepository.buscarEmpleadosporNombre(searchField);
+        List<Employees> listaEmpleados = employeesRepository.buscarEmpleadosporBuscador(searchField);
         model.addAttribute("listaEmpleados",listaEmpleados);
         return "employee/lista";
     }
